5ccf633dc72ca848beb26ff6d0747ffe60fcc3cd
kxiao1415 <k.xiao1415@gmail.com>
2017-12-22 23:47:18 -0500

    encrypted user phone and email columns

--- pyws/data/model/user_model.py
+++ pyws/data/model/user_model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from sqlalchemy_utils import EncryptedType
 
-from pyws import db
+from pyws import db, app
 from pyws.data.model.base_model import BaseModel
 from pyws.helper import data_helper
 
@@ -9,15 +10,15 @@ class UserModel(db.Model, BaseModel):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    first_name = db.Column(db.String(64), nullable=False)
-    last_name = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    phone = db.Column(db.Integer, index=True, unique=True)
+    user_name = db.Column(db.Unicode(64), index=True, unique=True, nullable=False)
+    first_name = db.Column(db.Unicode(64), nullable=False)
+    last_name = db.Column(db.Unicode(64), nullable=False)
+    email = db.Column(EncryptedType(db.Unicode(64), app.config['SECRET_KEY']), index=True, unique=True, nullable=False)
+    phone = db.Column(EncryptedType(db.Integer, app.config['SECRET_KEY']), index=True, unique=True)
     gender = db.Column(db.Enum('M', 'F', name='gender_enum'))
-    short_description = db.Column(db.String(140))
-    long_description = db.Column(db.Text)
-    education = db.Column(db.String(64))
+    short_description = db.Column(db.Unicode(140))
+    long_description = db.Column(db.UnicodeText)
+    education = db.Column(db.Unicode(64))
     estimated_age = db.Column(db.Integer)
     budget_max = db.Column(db.Integer)
     budget_min = db.Column(db.Integer)
